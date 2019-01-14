65c9c03a3aeee1f842bd6f15d4afd9e563c92a70
Naoko Nishimura <naoko.nishimura1018@gmail.com>
2019-01-14 14:11:19 +0900

    update

--- git.sh
+++ git.sh
@@ -4,8 +4,8 @@ echo "start!! herokuサーバーへ修正を反映します"
 echo "*****************************"
 echo ""
 
-git config user.name "Your Name"
-git config user.email "youremail@example.com"
+git config user.name "Naoko Nishimura"
+git config user.email "naoko.nishimura1018@gmail.com"
 git add .
 git commit -m "update"
 git push heroku master
